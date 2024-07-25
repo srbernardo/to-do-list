99131d4c1c4ffc6f167b1faf0b745da5fc73e099
bernardo brotelli <b.brotelle@gmail.com>
2024-07-25 17:27:04 -0300

    Config sequelize

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from 'express'
 
 import taskRoutes from './routes/taskRoutes.js'
+import sequelize from './database/database.js';
 
 const app = express()
 
 app.use(express.json())
 app.use(taskRoutes)
 
-app.listen(3000, () => {console.log('Rodando na porta 3000')})
+sequelize.sync().then(() => {
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+}).catch((err) => {
+  console.error('Unable to connect to the database:', err);
+});
--- src/database/config.js
+++
@@ -1,9 +0,0 @@
-import sqlite3 from "sqlite3";
-
-const db = new sqlite3.Database('./src/database/database.db')
-
-db.serialize(() => {
-  db.run("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT)");
-});
-
-export default db
